00e3949885f7fa05d812450657421de1c7d3dca0
Sophia Casper <uhvnl@student.kit.edu>
2026-02-14 17:13:28 +0100

    update wifi block

--- blocks/wifi.sh
+++ blocks/wifi.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 # dependency check
 if ! command -v nmcli >/dev/null; then
     echo "Wi-Fi: N/A"
@@ -9,26 +7,34 @@ if ! command -v nmcli >/dev/null; then
 fi
 
 # click opens network editor (optional)
-if [ "${BLOCK_BUTTON:-}" = "1" ]; then
+if [ "$BLOCK_BUTTON" = "1" ]; then
     if command -v nm-connection-editor >/dev/null; then
         nm-connection-editor >/dev/null 2>&1 &
     fi
 fi
 
-# check wifi enabled
+# check wifi enabled, if there is an error, then unknown is displayed
 wifi_state=$(nmcli -t -f WIFI g 2>/dev/null || echo "unknown")
+
+# wifi disabled
 if [ "$wifi_state" = "disabled" ]; then
     echo "Wi-Fi: Off"
     exit 0
 fi
 
-# current connection (active SSID)
+# current connection (active SSID) (get all ssid and filter the active ones)
 essid=$(nmcli -t -f active,ssid dev wifi 2>/dev/null \
   | awk -F: '$1=="yes"{print $2; exit}')
 
+# wifi not connected, but enabled
 if [ -z "$essid" ]; then
     echo "Wi-Fi: Disconnected"
 else
-    safe_essid=$(echo "$essid" | sed 's/\\/\\\\/g; s/"/\\"/g; s/%/%%/g')
+	# replace \ with \\
+    safe_essid=$(sed 's/"/\\"/g' <<< "$safe_essid")
+    # replace % with %%
+    safe_essid=$(sed 's/%/%%/g' <<< "$safe_essid")
+    # replace " with \"
+    safe_essid=$(sed 's/"/\\"/g' <<< "$safe_essid")
     echo "Wi-Fi: $safe_essid"
 fi
